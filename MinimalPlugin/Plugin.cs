71cf7073f0e06b18bfceaf4e42dc0eda25c9b3c9
Sylvie Nightshade <hi@sylvie.software>
2024-07-07 22:57:05 +0100

    Initial commit

---
+++ MinimalPlugin/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace MinimalPlugin;
+
+public class Plugin : IDalamudPlugin
+{
+    internal static Plugin Instance = null!;
+    
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+
+    public Plugin()
+    {
+        Instance = this;
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MinimalPlugin
+
+The simplest possible Dalamud plugin.
+
+This repository assumes you are familiar with Dalamud's APIs and structure. It provides almost no code and zero scaffolding, it's a total blank slate. Get out there and write some code quicker!
